8ee897763539f22e8e5c2563d36a23ebd292e640
Matej Janacek <matej.janacek@gmail.com>
2023-03-27 19:54:23 +0200

    Adding Title, small ui changes

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,10 @@
 import { Game } from './components/Game';
+import { Title } from './components/title/Title';
 
 export const App: React.FC = () => {
     return (
         <div className='app'>
+            <Title />
             <Game />
         </div>
     );
---
+++ react/src/components/title/Title.tsx
@@ -0,0 +1,5 @@
+import style from './title.module.css';
+
+export const Title: React.FC = () => {
+    return <h1 className={style.wrapper}>Gomoku</h1>;
+};
